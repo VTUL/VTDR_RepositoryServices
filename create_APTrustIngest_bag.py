931e736fb3d8c6f92745aef55639827933757d87
Padma Carstens <padma@vt.edu>
2022-06-17 11:36:38 -0400

    Publication bag creation with DART, bag transfer to APTrust, README automation

---
+++ create_APTrustIngest_bag.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  6 12:39:04 2021
+
+@author: padma carstens
+"""
+
+import os
+from os.path import exists
+from figshare.figshare import Figshare
+from UPack_v2 import ObjFormatter
+from ldcoolp.curation import retrieve
+import json
+from spreadsheet import vtingsheet
+from datetime import date
+import filecmp
+from datetime import datetime
+import job
+from job import Job
+#from fill_readme import create_readme
+
+filename="secrets.txt"
+fileObj=open(filename)
+params={}
+for line in fileObj:
+    line=line.strip()
+    key_value=line.split('=')
+    if len(key_value)==2:
+        params[key_value[0].strip()]=key_value[1].strip()
+
+
+#Get ArticleID from secrets.txt:
+ArticleID=params["ArticleID"]
+#Get the Published Version number from secrets.txt
+PublishedVersionNumber=params["PublishedVersionNumber"]
+#Get the Ingest Version number from secrets.txt
+IngestVersionNumber=params["IngestVersionNumber"]
+#Get your figshare token from secrets.txt
+token=params["token"]
+#Get curator name from secrets.txt
+#readme_auto=create_readme(ArticleID,token)
+CuratorName=params["CuratorName"]
+#Enter the ingest record creation number: (Is this the first time creating ingest record or nth time to check against the originial bag?)
+ingestrecord_creation_number=int(params["ingestrecord_creation_number"])
+#Get information from the spreadsheet for the corresponding ArticleID and Version Number:
+ingsheet=vtingsheet(ArticleID,IngestVersionNumber)
+#Get article id
+article_id=ingsheet['ingarticleid']
+# get Ingest Accession Number 
+IngestAccessionNumber=ingsheet['ingestno'] 
+#get Requestor name
+Requestor=ingsheet['ingrequestr']
+#get corresponding author name
+CorrespondingAuthor=ingsheet['ingrequestr']
+#Get LastnameFirstnameinitial of requestor and corresponding author:
+Requestorlfi=ingsheet['ingreqlastfirsti']
+CorrespondingAuthorlfi=ingsheet['ingcorlastfirsti']
+
+#get version number
+Version=ingsheet['ingversion']
+#get date ingested in YYYYMMDD format
+DateIngested= ingsheet['ingestdate']  
+#get current date
+today=date.today()
+date_current=today.strftime("%Y%m%d")
+#get current time
+now=datetime.now()
+time_current=now.strftime("%H_%M_%S")
+
+data_directory_path=f"VTDR_{IngestAccessionNumber}_AID_{article_id}_{CorrespondingAuthorlfi}_v{Version}_{DateIngested}"
+
+metadata_directory_path=f"{IngestAccessionNumber}_DownloadedFileMetadata"
+
+#-----Download dataset for private article under review using LD-Cool-P and save it as Ingest metadata in json file format
+fs=Figshare(token=token,private=True)
+FileDownload=retrieve.download_files(article_id, fs, data_directory=data_directory_path, metadata_directory=metadata_directory_path)
+#privatefigshare_url='https://api.figshare.com/v2/account/articles/'+str(article_id)
+privatefigshare_url='https://api.figshare.com/v'+str(Version[1])+'/account/articles/'+str(article_id)
+#-----Get article details for private article under review using LD-Cool-P and save it as Ingest metadata in json file format
+json_out_file=f"{data_directory_path}/{IngestAccessionNumber}_IngestedMetadata.json"
+json_response=fs.get_article_details(article_id,version=None)
+
+if not os.path.exists(json_out_file):
+    with open(json_out_file, 'w') as f:
+        json.dump(json_response,f,indent=4)
+else:
+    print(f"File exists: {json_out_file}")
+
+
+#bagging with DART using tar format
+
+payload=os.listdir(data_directory_path)
+aptrustBagName=data_directory_path
+job = Job("APTrust Demo Workflow for Virginia Tech",aptrustBagName)
+#job = Job("APTrust Production Workflow for Virginia Tech",aptrustBagName)
+for f in payload:
+    job.add_file(data_directory_path+"\\"+f)
+    print("Added following file to bag in DART: ",f)
+    bag_group_identifier=f"VTDR_{IngestAccessionNumber}"
+job.add_tag("bag-info.txt", "Bag-Group-Identifier", bag_group_identifier)
+job.add_tag("bag-info.txt","Source-Organization","Virginia Tech")
+job.add_tag("aptrust-info.txt", "Access", "Institution")
+job.add_tag("aptrust-info.txt", "Storage-Option", "Standard")
+aptrust_title=aptrustBagName
+job.add_tag("aptrust-info.txt","Title",aptrust_title)
+job.add_tag("bagit.txt","BagIt-Version","0.97")
+job.add_tag("bagit.txt","Tag-File-Character-Encoding","UTF-8")
+
+exit_code = job.run()
+if exit_code == 0:
+    print("Job completed")
+    print("**************************BAG MIGRATED SUCCESSFULLY TO APTRUST****************")
+
+else:
+    print("Job failed. Check the DART log for details.")
+    print("**************************BAG MIGRATION TO APTRUST FAILED****************")
+
+     
--- create_APTrustPublication_bag.py
+++ create_APTrustPublication_bag.py
@@ -12,13 +12,13 @@ import json
 from ldcoolp.curation import retrieve
 from auto_fill_archive import create_archivalreadme
 from spreadsheet import vtpubsheet
+from spreadsheet import vtingsheet
 import shutil
 import os
 #Get the parameters from secrets.txt created in the curation folder
-#filename="C:\Users\padma\anaconda3\envs\curation\secrets.txt"
 filename="secrets.txt"
 fileObj=open(filename)
-#fileObj=open(filename,'r')
+
 params={}
 for line in fileObj:
     line=line.strip()
@@ -36,6 +36,9 @@ IngestVersionNumber=params["IngestVersionNumber"]
 token=params["token"]
 #Get curator name from secrets.txt
 CuratorName=params["CuratorName"]
+#Get figshare version for published articles
+FigVersion=params["FigVersion"]
+
 #get the details from the spreadsheet:
 vtsheet=vtpubsheet(ArticleID,PublishedVersionNumber)
 #Get article id 
@@ -48,29 +51,35 @@ CorrespondingAuthor=vtsheet['gscorsauth']
 Version=vtsheet['gsversnum']
 #get published date in YYYYMMDD format 
 DatePublished= vtsheet['gsdatepub'] 
+#get DOI suffix
+DOIsuffix=vtsheet['DOIsuffix']
+
 
 #Create Publication folder to store dataset
 PublishedAccessionNumber= vtsheet['gspubnum']
 RequestorLFI=vtsheet['gsreqlastfi']
 CorrespondingAuthorLFI=vtsheet['gscorrlastfi']
 
+
+#************INGEST SHEET INFO
+ingsheet=vtingsheet(ArticleID,IngestVersionNumber)
+IngestAccessionNumber=ingsheet['ingestno'] 
+
+#********************************
 #Get current directory:
 directory_path=os.getcwd()  
 
-data_directory1=f"{PublishedAccessionNumber}_v{Version}"
-
-data_directory2=f"{PublishedAccessionNumber}_{RequestorLFI}_{CorrespondingAuthorLFI}_v{Version}_{DatePublished}"
-
-data_directory3=f"DisseminatedContent"
-
-data_directory_path=os.path.join(data_directory1, data_directory2, data_directory3)
+aptrustBagName=f"VTDR_{PublishedAccessionNumber}_{IngestAccessionNumber}_DOI_{DOIsuffix}_{CorrespondingAuthorLFI}_v{Version}_{DatePublished}"
+payload_directory1=f"DisseminatedContent"
+data_directory_path=os.path.join(aptrustBagName, payload_directory1)
 metadata_directory_path=f"{PublishedAccessionNumber}_DownloadedFileMetadata_v{Version}"
 
 #-----Download dataset for published article using LD-Cool-P UAL and save it as publication meta data in json file format
-publicfigshare_url='https://api.figshare.com/v1/articles/'+str(article_id)
+publicfigshare_url='https://api.figshare.com/'+str(FigVersion[1])+'/articles/'+str(article_id)
 from figshare.figshare import Figshare
 fs=Figshare(token=token,private=False)
 FileDownload=retrieve.download_files(article_id, fs, data_directory=data_directory_path, metadata_directory=metadata_directory_path)
+
 #-----get article details for published article using LD-Cool-P and save it as published metadata in json file format
 json_out_file1=f"{data_directory_path}/{PublishedAccessionNumber}_DisseminatedMetadata.json"
 json_response1=fs.get_article_details(article_id,version=None)
@@ -82,12 +91,15 @@ if not os.path.exists(json_out_file1):
 else:
     print(f"File exists: {json_out_file1}")
 
-archival_directory=os.path.join(data_directory1,data_directory2)
+archival_directory=os.path.join(aptrustBagName)
 #create archival readme file using auto_fill_archive.py
+
 reme=create_archivalreadme(ArticleID,PublishedVersionNumber,IngestVersionNumber,CuratorName,archival_directory) 
 #----------------------create VTCurationServicesActions folder to save provenance log and email correspondence
                 
-data_directory4=f"VTCurationServicesActions"     
-data_directory_path2=os.path.join(data_directory1,data_directory2,data_directory4)
-os.mkdir(data_directory_path2)
-print("Directory '% s' created" % data_directory4) 
+payload_directory2=f"VTCurationServicesActions"     
+#data_directory_path2=os.path.join(data_directory1,data_directory2,data_directory4)
+payload_path=os.path.join(aptrustBagName,payload_directory2)
+os.mkdir(payload_path)
+print("Directory '% s' created" % payload_path) 
+
